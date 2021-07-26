b275fef99292a9f2cfe5b915827a994eb9a32ec4
Leah Garrison <lgarrison1023@gmail.com>
2021-07-26 10:04:24 -0400

    added mailchimp add and renamed tapestry-snowflake.js to snowflake.js

---
+++ config/apps/mailchimp.js
@@ -0,0 +1,20 @@
+// const {mailchimpApiKey,  mailchimpAppId} = require('../../getConfigInputs.js');
+
+// const {mailchimpApiKey,  mailchimpAppId} = require('../../getConfigInputs.js');
+
+
+const mailchimpApiKey = process.env.MAILCHIMP_API_KEY
+exports.default = async function buildConfig() {
+  return [
+    {
+      class: "app",
+      id:  mailchimpAppId,
+      name:  mailchimpAppId,
+      type: "mailchimp",
+      options: {
+        apiKey: mailchimpApiKey, // Mailchimp API key
+        // {{ssm:MAILCHIMP_API_KEY}}
+      },
+    },
+  ];
+};
