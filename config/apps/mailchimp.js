ba0f59e7dc93b767da6dc211c37912962040d8cf
Leah Garrison <lgarrison1023@gmail.com>
2021-07-26 10:16:30 -0400

    added mailchimp destination

--- config/apps/mailchimp.js
+++ config/apps/mailchimp.js
@@ -2,7 +2,7 @@
 
 // const {mailchimpApiKey,  mailchimpAppId} = require('../../getConfigInputs.js');
 
-
+// 830405a4f4d726ab4b5e20be73c5a092-us6
 const mailchimpApiKey = process.env.MAILCHIMP_API_KEY
 exports.default = async function buildConfig() {
   return [
