4012c8f977cb7625e63745560be132e21ea05526
Leah Garrison <lgarrison1023@gmail.com>
2021-07-26 09:44:54 -0400

    reset code; pulled from grouparoo-config-deploy

--- config/apps/tapestry_snowflake.js
+++ config/apps/tapestry_snowflake.js
@@ -1,32 +1,3 @@
-// const { 
-//   snowAcctHost,
-//   snowUserAcctUser,
-//   snowAcctPass,
-//   snowflakeAppId,
-//   snowflakeWarehouseName,
-//   snowflakeDatabaseName,
-//   snowflakeSchemaName
-// } = require('../../getConfigInputs.js');
-
-// exports.default = async function buildConfig() {
-//   return [
-//     {
-//       class: "app",
-//       id: snowflakeAppId,
-//       name:  snowflakeAppId,
-//       type: "snowflake",
-//       options: {
-//         account: snowAcctHost,
-//         username: snowUserAcctUser,
-//         password: snowAcctPass ,
-//         warehouse: snowflakeWarehouseName, // The Snowflake warehouse to use - e.g. `warehouse: "COMPUTE_WH"`
-//         database: snowflakeDatabaseName, // The Snowflake database to use
-//         schema:  snowflakeSchemaName, // The Snowflake schema (default: PUBLIC)
-//       },
-//     },
-//   ];
-// };
-
 exports.default = async function buildConfig() {
   // const account = JSON.parse(execSync('aws ssm get-parameter --name "/snowflake/acct-hostname" --with-decryption').toString()).Parameter.Value;
   // const username = JSON.parse(execSync('aws ssm get-parameter --name "/snowflake/acct-username" --with-decryption').toString()).Parameter.Value;
@@ -55,4 +26,3 @@ exports.default = async function buildConfig() {
     },
   ];
 }
-
